8273479d306a8442b268acc813d6c70aae689601
Sadeepa Dhananjaya <sadeepaekanayaked@gmail.com>
2023-07-26 22:38:10 +0530

    Exhibit View

--- src/components/dashboard/ExhibitView.js
+++ src/components/dashboard/ExhibitView.js
@@ -2,7 +2,8 @@ import React, { Fragment, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { projectGet } from "../../models/project";
 import Spinner from "../layout/Spinner";
-import { useNavigate } from "react-router-dom";
+import { useNavigate,Link } from "react-router-dom";
+import MapContainer from "../utils/Map";
 
 const ExhibitView = () => {
   const [formData, setFormData] = useState({
@@ -23,24 +24,24 @@ const ExhibitView = () => {
     userid: "",
     displayImage: "",
   });
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
   const { id } = useParams();
-  console.log(formData);
   const {
     title,
     caption,
-    faculty,
     department,
     displayImage,
     description,
+    location,
+    team,
     images,
   } = formData;
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
       const data = await projectGet(id);
-      // console.log(data);
+      console.log(data);
       setFormData(data);
       setIsLoading(false);
     };
@@ -54,7 +55,13 @@ const ExhibitView = () => {
         type: "exhibits",
       },
     });
-  }
+  };
+  const handleMapLocationChange = (latitude, longitude) => {
+    setFormData({
+      ...formData,
+      location: { ...location, latitude, longitude },
+    });
+  };
   return (
     <div>
       <div className="card larger">
@@ -69,16 +76,27 @@ const ExhibitView = () => {
               <div className="details">
                 <div className="large text-primary">{title}</div>
                 <p className="caption">{caption}</p>
+                <p className="caption">{department}</p>
+                {/* show each team member in 'team' separated by "|" */}
+                
+                
                 <p className="caption">
-                  {faculty} | {department}
+                  {team.map((member,index) => (
+                    index===0?member:<span> | {member} </span>
+                  ))}
                 </p>
-                <p className="caption">Team Member 1 | Team Member 2</p>
-                <button className="btn btn-primary mt-1" onClick={()=>onEdit()}>
+                <button
+                  className="btn btn-primary mt-1"
+                  onClick={() => onEdit()}
+                >
                   <i className="fa fa-marker" aria-hidden="true"></i>
                 </button>
               </div>
             </div>
             <p className="description">{description}</p>
+            <div className="location">
+              <MapContainer onMapLocationChange={handleMapLocationChange} />
+            </div>
             {/* <div className="location">
               <p>Location</p>
             </div> */}
@@ -87,6 +105,9 @@ const ExhibitView = () => {
               <img src="./606224.png" alt="" />
               <img src="./606224.png" alt="" />
             </div>
+            <Link to="/exhibits" className="btn btn-light mt-1">
+            Go Back
+          </Link>
           </Fragment>
         )}
       </div>
